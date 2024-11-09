e146e1314616d9abed82d7496591c9befe5d7cd0
FayeSpica <fayespica@outlook.com>
2024-11-09 16:00:11 +0800

    d119

--- java/src/main/java/org/faye/daily/y2024/m11/D9.java
+++ java/src/main/java/org/faye/daily/y2024/m11/D9.java
@@ -20,53 +20,111 @@ public class D9 {
             rank[i] = 1;
         }
 
-        // 判断是否存在(AB -> BD | AC -> CD)
+        // 判断是否存在(AB -> BD | AC -> CD | AC -> BD | AB -> CD)
         // cn + 0 -> cn + 1
         // cn + 2 -> cn + 3
+        // cn + 2 -> cn + 1
+        // cn + 0 -> cn + 3
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i < cn) {
                     if (j < cn) {
                         // 两个圆
                         long length = ((long)Math.abs(circles[i][0] - circles[j][0]))*((long)Math.abs(circles[i][0] - circles[j][0])) + ((long)Math.abs(circles[i][1] - circles[j][1])) * ((long)Math.abs(circles[i][1] - circles[j][1]));
-                        if (length <= ((long)(circles[i][2] + circles[j][2])) * ((long)(circles[i][2] + circles[j][2])) ) {
-                            union(i, j);
+                        // 如果两个圆都在外面，需要重叠部分在坐标内
+                        boolean iOuter = !((circles[i][0] >= 0 && circles[i][0] <= xCorner) && (circles[i][1] >= 0 && circles[i][1] <= yCorner));
+                        boolean jOuter = !((circles[j][0] >= 0 && circles[j][0] <= xCorner) && (circles[j][1] >= 0 && circles[j][1] <= yCorner));
+
+                        if (length <= ((long) (circles[i][2] + circles[j][2])) * ((long) (circles[i][2] + circles[j][2]))) {
+                            if (iOuter && jOuter) {
+                                // 计算点A 在内部
+                                double aX = (double) (circles[i][0] * circles[j][2] + circles[j][0] * circles[i][2]) /(double) (circles[i][2] + circles[j][2]);
+                                double aY = (double) (circles[i][1] * circles[j][2] + circles[j][1] * circles[i][2]) /(double) (circles[i][2] + circles[j][2]);
+                                if ((aX >= 0 && aX <= xCorner) && (aY >= 0 && aY <= yCorner)) {
+                                    union(i, j);
+                                }
+                            } else {
+                                union(i, j);
+                            }
                         }
                     } else {
                         if (j == cn) {
                             // AB
-                            long ab_length = yCorner - circles[i][1];
-                            long bd_length = xCorner - circles[i][0];
-                            if (ab_length < 0 || bd_length < 0) { // 圆心在矩形外 需要同时满足 AB 与 BD length > -r
-                                if (ab_length > -circles[i][2] && (bd_length > -circles[i][2] || bd_length >= 0)) {
-                                    union(i, j);
-                                }
+                            // 圆心投射到AB上的点p1
+                            // 如果 在AB区间内 只要p1到圆心的距离 <= r
+                            // 如果 在AB区间外 需要边界到圆心的距离 < r
+                            // 外切圆不要
+                            int projectionX = circles[i][0];
+                            int projectionY = yCorner;
+                            long length;
+                            boolean outer = !((circles[i][0] >= 0 && circles[i][0] <= xCorner) && (circles[i][1] >= 0 && circles[i][1] <= yCorner));
+                            if (0 <= projectionX && projectionX <= xCorner) {
+
+                            } else if (projectionX < 0) {
+                                projectionX = 0;
+                            } else {
+                                projectionX = xCorner;
                             }
-                            else if (ab_length <= circles[i][2]) {
+                            length = ((long)Math.abs(circles[i][0] - projectionX))*((long)Math.abs(circles[i][0] - projectionX)) + ((long)Math.abs(circles[i][1] - projectionY)) * ((long)Math.abs(circles[i][1] - projectionY));
+                            long r2 = ((long)circles[i][2]) * ((long)circles[i][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (j == cn + 1) {
                             // BD
-                            long ab_length = yCorner - circles[i][1];
-                            long bd_length = xCorner - circles[i][0];
-                            if (ab_length < 0 || bd_length < 0) { // 圆心在矩形外 需要同时满足 x + r < xCorner  与y + r < yCorner
-                                if (bd_length > -circles[i][2] && (ab_length > -circles[i][2] || ab_length >= 0)) {
-                                    union(i, j);
-                                }
+                            int projectionX = xCorner;
+                            int projectionY = circles[i][1];
+                            long length;
+                            boolean outer = !((circles[i][0] >= 0 && circles[i][0] <= xCorner) && (circles[i][1] >= 0 && circles[i][1] <= yCorner));
+                            if (0 <= projectionY && projectionY <= yCorner) {
+
+                            } else if (projectionY < 0) {
+                                projectionY = 0;
+                            } else {
+                                projectionY = yCorner;
                             }
-                            else if (bd_length <= circles[i][2]) { // 圆心在矩形内
+                            length = ((long)Math.abs(circles[i][0] - projectionX))*((long)Math.abs(circles[i][0] - projectionX)) + ((long)Math.abs(circles[i][1] - projectionY)) * ((long)Math.abs(circles[i][1] - projectionY));
+
+                            long r2 = ((long)circles[i][2]) * ((long)circles[i][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (j == cn + 2) {
                             // AC
-                            long length = circles[i][0];
-                            if (length <= circles[i][2]) {
+                            int projectionX = 0;
+                            int projectionY = circles[i][1];
+                            long length;
+                            boolean outer = !((circles[i][0] >= 0 && circles[i][0] <= xCorner) && (circles[i][1] >= 0 && circles[i][1] <= yCorner));
+                            if (0 <= projectionY && projectionY <= yCorner) {
+
+                            } else if (projectionY < 0) {
+                                projectionY = 0;
+                            } else {
+                                projectionY = yCorner;
+                            }
+                            length = ((long)Math.abs(circles[i][0] - projectionX))*((long)Math.abs(circles[i][0] - projectionX)) + ((long)Math.abs(circles[i][1] - projectionY)) * ((long)Math.abs(circles[i][1] - projectionY));
+
+                            long r2 = ((long)circles[i][2]) * ((long)circles[i][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (j == cn + 3) {
                             // CD
-                            long length = circles[i][1];
-                            if (length <= circles[i][2]) {
+                            int projectionX = circles[i][0];
+                            int projectionY = 0;
+                            long length;
+                            boolean outer = !((circles[i][0] >= 0 && circles[i][0] <= xCorner) && (circles[i][1] >= 0 && circles[i][1] <= yCorner));
+                            if (0 <= projectionX && projectionX <= xCorner) {
+
+                            } else if (projectionX < 0) {
+                                projectionX = 0;
+                            } else {
+                                projectionX = xCorner;
+                            }
+                            length = ((long)Math.abs(circles[i][0] - projectionX))*((long)Math.abs(circles[i][0] - projectionX)) + ((long)Math.abs(circles[i][1] - projectionY)) * ((long)Math.abs(circles[i][1] - projectionY));
+
+                            long r2 = ((long)circles[i][2]) * ((long)circles[i][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         }
@@ -75,38 +133,78 @@ public class D9 {
                     if (j < cn) {
                         if (i == cn) {
                             // AB
-                            long ab_length = yCorner - circles[j][1];
-                            long bd_length = xCorner - circles[j][0];
-                            if (ab_length < 0 || bd_length < 0) { // 圆心在矩形外 需要同时满足 AB 与 BD length > -r
-                                if (ab_length > -circles[j][2] && (bd_length > -circles[j][2] || bd_length >= 0)) {
-                                    union(i, j);
-                                }
+                            int projectionX = circles[j][0];
+                            int projectionY = yCorner;
+                            long length;
+                            boolean outer = !((circles[j][0] >= 0 && circles[j][0] <= xCorner) && (circles[j][1] >= 0 && circles[j][1] <= yCorner));
+                            if (0 <= projectionX && projectionX <= xCorner) {
+
+                            } else if (projectionX < 0) {
+                                projectionX = 0;
+                            } else {
+                                projectionX = xCorner;
                             }
-                            else if (ab_length <= circles[j][2]) {
+                            length = ((long)Math.abs(circles[j][0] - projectionX))*((long)Math.abs(circles[j][0] - projectionX)) + ((long)Math.abs(circles[j][1] - projectionY)) * ((long)Math.abs(circles[j][1] - projectionY));
+
+                            long r2 = ((long)circles[j][2]) * ((long)circles[j][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (i == cn + 1) {
                             // BD
-                            long ab_length = yCorner - circles[j][1];
-                            long bd_length = xCorner - circles[j][0];
-                            if (ab_length < 0 || bd_length < 0) { // 圆心在矩形外 需要同时满足 x + r < xCorner  与y + r < yCorner
-                                if (bd_length > -circles[j][2] && (ab_length > -circles[j][2] || ab_length >= 0)) {
-                                    union(i, j);
-                                }
+                            int projectionX = xCorner;
+                            int projectionY = circles[j][1];
+                            long length;
+                            boolean outer = !((circles[j][0] >= 0 && circles[j][0] <= xCorner) && (circles[j][1] >= 0 && circles[j][1] <= yCorner));
+                            if (0 <= projectionY && projectionY <= yCorner) {
+
+                            } else if (projectionY < 0) {
+                                projectionY = 0;
+                            } else {
+                                projectionY = yCorner;
                             }
-                            else if (bd_length <= circles[j][2]) { // 圆心在矩形内
+                            length = ((long)Math.abs(circles[j][0] - projectionX))*((long)Math.abs(circles[j][0] - projectionX)) + ((long)Math.abs(circles[j][1] - projectionY)) * ((long)Math.abs(circles[j][1] - projectionY));
+
+                            long r2 = ((long)circles[j][2]) * ((long)circles[j][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (i == cn + 2) {
                             // AC
-                            long length = circles[j][0];
-                            if (length <= circles[j][2]) {
+                            int projectionX = 0;
+                            int projectionY = circles[j][1];
+                            long length;
+                            boolean outer = !((circles[j][0] >= 0 && circles[j][0] <= xCorner) && (circles[j][1] >= 0 && circles[j][1] <= yCorner));
+                            if (0 <= projectionY && projectionY <= yCorner) {
+
+                            } else if (projectionY < 0) {
+                                projectionY = 0;
+                            } else {
+                                projectionY = yCorner;
+                            }
+                            length = ((long)Math.abs(circles[j][0] - projectionX))*((long)Math.abs(circles[j][0] - projectionX)) + ((long)Math.abs(circles[j][1] - projectionY)) * ((long)Math.abs(circles[j][1] - projectionY));
+
+                            long r2 = ((long)circles[j][2]) * ((long)circles[j][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         } else if (i == cn + 3) {
                             // CD
-                            long length = circles[j][1];
-                            if (length <= circles[j][2]) {
+                            int projectionX = circles[j][0];
+                            int projectionY = 0;
+                            long length;
+                            boolean outer = !((circles[j][0] >= 0 && circles[j][0] <= xCorner) && (circles[j][1] >= 0 && circles[j][1] <= yCorner));
+                            if (0 <= projectionX && projectionX <= xCorner) {
+
+                            } else if (projectionX < 0) {
+                                projectionX = 0;
+                            } else {
+                                projectionX = xCorner;
+                            }
+                            length = ((long)Math.abs(circles[j][0] - projectionX))*((long)Math.abs(circles[j][0] - projectionX)) + ((long)Math.abs(circles[j][1] - projectionY)) * ((long)Math.abs(circles[j][1] - projectionY));
+
+                            long r2 = ((long)circles[j][2]) * ((long)circles[j][2]);
+                            if ((outer && length <= r2) || ((!outer) && length <= r2)) {
                                 union(i, j);
                             }
                         }
@@ -117,7 +215,7 @@ public class D9 {
             }
         }
 
-        return !(find(cn + 0) == find(cn + 1) || find(cn + 2) == find(cn + 3));
+        return !(find(cn + 0) == find(cn + 1) || find(cn + 2) == find(cn + 3) || find(cn + 2) == find(cn + 1) || find(cn + 0) == find(cn + 3));
     }
 
     public int find(int x) {
@@ -132,6 +230,7 @@ public class D9 {
         int rootY = find(y);
 
         if (rootX != rootY) {
+            System.out.println(x + " " + y);
             if (rank[rootX] > rank[rootY]) {
                 parent[rootY] = rootX;
             } else if (rank[rootX] < rank[rootY]) {
@@ -144,6 +243,10 @@ public class D9 {
     }
 
     public static void main(String[] args) {
-        new D9().canReachCorner(3, 3, new int[][]{{2, 1000, 997}, {1000, 2, 997}});
+        System.out.println(new D9().canReachCorner(3, 3, new int[][]{{2, 1000, 997}, {1000, 2, 997}}));;
+        System.out.println(new D9().canReachCorner(15, 15, new int[][]{{1, 99, 85}, {99, 1, 85}}));;
+        System.out.println(new D9().canReachCorner(5, 8, new int[][]{{4, 7, 1}}));;
+        System.out.println(new D9().canReachCorner(8, 8, new int[][]{{1, 4, 1}, {3, 4, 1}, {5, 4, 1}, {7, 4, 1}}));;
+        System.out.println(new D9().canReachCorner(3, 3, new int[][]{{7, 6, 5}}));;
     }
 }
