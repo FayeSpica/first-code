25b28ecd7c042dc5197083b88fb193a9cfa5e8c6
FayeSpica <fayespica@outlook.com>
2024-11-13 01:32:16 +0800

    c419

---
+++ java/src/main/java/org/faye/c419/P1.java
@@ -0,0 +1,68 @@
+package org.faye.c419;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class P1 {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int[] res = new int[nums.length - k + 1];
+        int[] counter = new int[51];
+
+        int start = 0;
+        int end = k - 1;
+
+        for (int i = start; i < end; i++) {
+            counter[nums[i]]++;
+        }
+
+        int i = 0;
+
+        while (end < nums.length) {
+            counter[nums[end]]++;
+            System.out.println(Arrays.toString(counter));
+            res[i++] = count(counter, x);
+            counter[nums[start]]--;
+            start++;
+            end++;
+        }
+
+        return res;
+    }
+
+    static class T {
+        public int value;
+        public int index;
+
+        public T(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        public int x() {
+            return index * value;
+        }
+    }
+
+    public int count(int[] counter, int x) {
+        int sum = 0;
+        PriorityQueue<T> queue = new PriorityQueue<>((a, b) -> {
+            if (a.value == b.value) {
+                return - a.index + b.index;
+            } else {
+                return - a.value + b.value;
+            }
+        });
+        for (int i = 50; i > 0; i--) {
+            if (counter[i] > 0) {
+                queue.add(new T(counter[i], i));
+            }
+        }
+
+        while (x > 0 && !queue.isEmpty()) {
+            sum += queue.poll().x();
+            x--;
+        }
+
+        return sum;
+    }
+}
---
+++ java/src/main/java/org/faye/c419/P2.java
@@ -0,0 +1,61 @@
+package org.faye.c419;
+
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+ }
+
+public class P2 {
+
+    public int kthLargestPerfectSubtree(TreeNode root, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(value -> -value));
+        count(queue, root, k, true);
+        if (queue.size() < k) {
+            return -1;
+        }
+        while (k > 1) {
+            queue.poll();
+            k--;
+        }
+        return queue.peek();
+    }
+
+    public int[] count(PriorityQueue<Integer> queue, TreeNode root, int k, boolean twoLeaf) {
+        int[] count = new int[2]; // dep  count
+        if (root == null) {
+            return count;
+        }
+
+        int[] left = count(queue, root.left, k, root.left != null && root.right != null);
+        int[] right = count(queue, root.right, k, root.left != null && root.right != null);
+        count[0] = Math.max(left[0], right[0]) + 1;
+        count[1] = left[1] + right[1] + 1;
+        System.out.println("depth:" + count[0] + " count:" + count[1]);
+        if (twoLeaf && Math.pow(2, count[0] + 1) - 1 == count[1]) {
+            queue.add(count[1]);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(value -> -value));
+        queue.add(1);
+        queue.add(3);
+        queue.add(2);
+
+        System.out.println(queue.peek());
+    }
+}
