08589161922c58a5c489e28311278db406b4e42e
FayeSpica <fayespica@outlook.com>
2025-01-01 22:49:19 +0800

    p3255

---
+++ java/src/main/java/org/faye/endlesscheng/t1/s6_1/P2765.java
@@ -0,0 +1,19 @@
+package org.faye.endlesscheng.t1.s6_1;
+
+public class P2765 {
+    public int alternatingSubarray(int[] nums) {
+
+        int left = 0;
+        int right = 1;
+
+        int first = nums[left];
+
+        while (right < nums.length) {
+            int dis = right - left;
+
+
+        }
+
+        return 0;
+    }
+}
---
+++ java/src/main/java/org/faye/endlesscheng/t1/s6_1/P467.java
@@ -0,0 +1,7 @@
+package org.faye.endlesscheng.t1.s6_1;
+
+public class P467 {
+    public int findSubstringInWraproundString(String s) {
+        return 0;
+    }
+}
